4462d9fca8d52c4c1369d01a64e6e2545c809556
Rifjan29 <rjndla@gmail.com>
2021-11-19 16:50:06 +0700

    test

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -66,7 +67,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
         <script src="{{ asset('backend/assets/js/main.js') }}"></script>
-
 
         <!--Local Stuff-->
         <script>

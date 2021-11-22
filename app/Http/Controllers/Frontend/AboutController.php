4530122d188837573edc2377f721b5d4805fac48
Rifjan29 <rjndla@gmail.com>
2021-11-23 02:03:21 +0700

    Profil
    Yok semangat

--- app/Http/Controllers/Frontend/AboutController.php
+++ app/Http/Controllers/Frontend/AboutController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\File;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
-    //
+    private $param;
+    public function index()
+    {
+        return view('profil.profil');
+    }
 }

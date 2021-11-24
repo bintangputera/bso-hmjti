18afef6bd58d966cd1e3c85a9976d073b2d0d3fe
Bintang Putera <sangbintang917@gmail.com>
2021-11-24 08:06:34 +0700

    fixxx finish
    merapikan pada bagian halaman profile

--- resources/views/profil/profil.blade.php
+++ resources/views/profil/profil.blade.php
@@ -13,43 +13,43 @@
     <div class="row justify-content-center">
         <div class="col-lg-8 col-md-12 text-center">
           <h2 class="mb-3 mt-5">Sejarah BSO HMJTI</h2>
-          <p>
+          <p style="line-height: 1.0cm">
             {{ $sejarah }}
           </p>
         </div>
         <div class="col-lg-8 col-md-12 text-center">
             <h2 class="mb-3 mt-5">Visi HMJTI</h2>
-            <p>
+            <p style="line-height: 1.0cm">
                 {{ $content->visi }}
             </p>
         </div>
         <div class="col-lg-8 col-md-12">
             <h2 class="mb-3 mt-5 text-center">Misi HMJTI</h2>
-            <p class="card-text mb-4">
+            <p class="card-text mb-4" style="line-height: 1.0cm">
                 {!! $content->misi !!}
             </p>
         </div>
         <div class="col-lg-8 col-md-12 text-center">
             <h2 class="mb-3 mt-5">Motto HMJTI</h2>
-            <p>
+            <p style="line-height: 1.0cm">
                 {{ $content->moto }}
             </p>
             </div>
         <div class="col-lg-8 col-md-12 text-center">
             <h2 class="mb-3 mt-5">Tujuan BSO HMJTI</h2>
-            <p>
+            <p style="line-height: 1.0cm">
                 {{ $tujuan }}
             </p>
         </div>
         <div class="col-lg-8 col-md-12 text-center">
             <h2 class="mb-3 mt-5">Fungsi BSO HMJTI</h2>
-            <p>
+            <p style="line-height: 1.0cm">
                 {{ $fungsi }}
             </p>
         </div>
         <div class="col-lg-8 col-md-12 text-center">
             <h2 class="mb-3 mt-5">Tugas BSO HMJTI</h2>
-            <p>
+            <p style="line-height: 1.0cm; text-align:center">
                 {{ $tugas }}
             </p>
         </div>
